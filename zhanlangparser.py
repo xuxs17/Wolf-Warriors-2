d039c252b6dbce7af7d44cd7bcfe4d1252498a97
xuxs17 <xuxiangshang15@mails.ucas.ac.cn>
2017-09-01 13:34:32 +0800

    Update zhanlangparser.py

--- zhanlangparser.py
+++ zhanlangparser.py
@@ -4,15 +4,20 @@ Created on Tue Aug 29 15:52:28 2017
 
 @author: changjin
 """
-
+#载入模块
 from bs4 import BeautifulSoup
 
-class movieparser(): 
-    
+#创建类
+class movieparser():   
+    #获取豆瓣评论的方法
     def get_douban_comments(res):
-        comments_list = []  # 评论列表
+        #评论列表
+        comments_list = []  
+        #使用BeautifulSoup解析Html
         soup = BeautifulSoup(res)
+        #获取对应评论标签的内容
         comment_nodes = soup.select('.comment > p')
+        #将评论内容逐条添加进评论列表
         for node in comment_nodes:           
             comments_list.append(node.get_text().strip().replace("\n", "") + u'\n')
         return comments_list
